6eb871deaced5345b9d3e5de9a6ddacd179427aa
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-02-16 09:54:34 -0600

    changes in css and index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,14 +6,17 @@ import { History } from "../models/History.js"
 import { Series } from "../models/Series.js"
 
    
-    const domain = "https://gateway.marvel.com/v1/public/characters?limit=32&"
+    const domain = "https://gateway.marvel.com/v1/public/characters?limit=33&"
     const keyPublic = "ts=10&apikey=3d5d8db1f8cc9f4fd5a0a55547f709f7"
     const hash = "&hash=c698016c4752d96a67869332119dc245"
 
     const url = domain+keyPublic+hash
     console.log(url)
 
-    fetch(url)
+    const loadData = document.getElementById("getElements")
+    loadData.addEventListener("click",()=>{
+
+        fetch(url)
     .then((response) => response.json())
     .then(data => {
 
@@ -50,11 +53,10 @@ import { Series } from "../models/Series.js"
                 listCharacter.addCharacter(character)
             })
     })
-
+    })
   
     let cardCharacters = document.getElementById("card-Characters")
-
-    let btn = document.getElementById("getElements")
+    let btn = document.getElementById("printElements")
     btn.addEventListener("click",()=>{
         listCharacter.getCharacters().forEach(item => {
             let cards = document.createElement("article")
@@ -74,21 +76,102 @@ import { Series } from "../models/Series.js"
             imgContainer.appendChild(img)
             cards.appendChild(imgContainer)
 
-            item.getComics().forEach(comic =>{
-                console.log(`Comics: ${comic.getName()}`)
+
+            //BOTON PARA VER COMICS
+            let btnViewComics = document.createElement("button")
+            btnViewComics.classList.add("btn-comics")
+            btnViewComics.innerText = "Ver comics"
+            btnViewComics.addEventListener("click",()=>{
+                let modal = document.createElement("div")
+                modal.classList.add("modal")
+                let ul = document.createElement("ul")
+                ul.classList.add("ul-modal")
+                item.getComics().forEach(comic =>{
+                let li = document.createElement("li")
+                li.classList.add("li-modal")
+                li.innerText = comic.getName()
+                ul.appendChild(li)
             })
 
-            item.getEvents().forEach(comic =>{
-                console.log(`Events: ${comic.getName()}`)
+            modal.appendChild(ul)
+            document.body.appendChild(modal)
+            modal.addEventListener("click",()=>{
+                modal.remove()
+            })
+            })
+            cards.appendChild(btnViewComics)
+
+            //BOTON PARA VER EVENTOS
+            let btnViewEvents = document.createElement("button")
+            btnViewEvents.classList.add("btn-events")
+            btnViewEvents.innerText = "Ver eventos"
+            btnViewEvents.addEventListener("click",()=>{
+                let modal = document.createElement("div")
+                modal.classList.add("modal")
+                let ul = document.createElement("ul")
+                ul.classList.add("ul-modal")
+                item.getEvents().forEach(comic =>{
+                let li = document.createElement("li")
+                li.classList.add("li-modal")
+                li.innerText = comic.getName()
+                ul.appendChild(li)
             })
 
-            item.getHistories().forEach(comic =>{
-                console.log(`Hostories: ${comic.getName()}`)
+            modal.appendChild(ul)
+            document.body.appendChild(modal)
+            modal.addEventListener("click",()=>{
+                modal.remove()
+            })
+            })
+            cards.appendChild(btnViewEvents)
+
+            //BOTON PARA VER HISTORIAS
+            let btnViewHistories = document.createElement("button")
+            btnViewHistories.classList.add("btn-histories")
+            btnViewHistories.innerText = "Ver historias"
+            btnViewHistories.addEventListener("click",()=>{
+                let modal = document.createElement("div")
+                modal.classList.add("modal")
+                let ul = document.createElement("ul")
+                ul.classList.add("ul-modal")
+                item.getHistories().forEach(comic =>{
+                let li = document.createElement("li")
+                li.classList.add("li-modal")
+                li.innerText = comic.getName()
+                ul.appendChild(li)
             })
 
-            item.getSeries().forEach(comic =>{
-                console.log(`Series: ${comic.getName()}`)
+            modal.appendChild(ul)
+            document.body.appendChild(modal)
+            modal.addEventListener("click",()=>{
+                modal.remove()
             })
+            })
+            cards.appendChild(btnViewHistories)
+
+            //BOTON PARA VER SERIES
+            let btnViewSeries = document.createElement("button")
+            btnViewSeries.classList.add("btn-series")
+            btnViewSeries.innerText = "Ver series"
+            btnViewSeries.addEventListener("click",()=>{
+                let modal = document.createElement("div")
+                modal.classList.add("modal")
+                let ul = document.createElement("ul")
+                ul.classList.add("ul-modal")
+                item.getSeries().forEach(comic =>{
+                let li = document.createElement("li")
+                li.classList.add("li-modal")
+                li.innerText = comic.getName()
+                ul.appendChild(li)
+            })
+
+            modal.appendChild(ul)
+            document.body.appendChild(modal)
+            modal.addEventListener("click",()=>{
+                modal.remove()
+            })
+            })
+            cards.appendChild(btnViewSeries)
 
             cardCharacters.appendChild(cards)
         })
